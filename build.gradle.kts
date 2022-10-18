fbfb40cce85792b228512c98f82c0a126b7be054
Michael Akinde <michael.akinde@statnett.no>
2022-10-18 11:02:21 +0200

    Update sonar-phab with elhub-gradle-plugins
    Summary: - [minor] update
    
    Test Plan: ./gradlew clean test
    
    Reviewers: O5 elhub/devxp, sergei.prodanov
    
    Reviewed By: O5 elhub/devxp, sergei.prodanov
    
    Subscribers: sergei.prodanov, teamcity
    
    Differential Revision: https://phabricator.elhub.cloud/D8094

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,41 +1,33 @@
+plugins {
+    id("no.elhub.devxp.kotlin-application") version "0.0.2"
+}
+
 description = "Retrieve SonarScan results from Sonarqube and post them to Phabricator."
 
-val kotestVersion = "4.3.2"
-val jacksonVersion = "2.11.3"
-
 dependencies {
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.apache.httpcomponents:httpclient:4.3.6")
-    implementation("info.picocli:picocli:4.6.1")
-    implementation("org.slf4j:slf4j-api:1.7.30")
-    implementation("org.slf4j:slf4j-simple:1.7.30")
-    implementation("commons-io:commons-io:2.8.0")
-    implementation("org.json:json:20200518")
-    implementation("com.fasterxml.jackson.core:jackson-core:$jacksonVersion")
-    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
-    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
-    testImplementation("io.kotest:kotest-extensions-allure-jvm:$kotestVersion")
+    implementation(platform(libs.elhub.devxp.bom))
+    implementation(platform(libs.jetbrains.kotlin.bom))
+    implementation(libs.jetbrains.kotlin.stdlibJdk8)
+    implementation(libs.logging.slf4JApi)
+    implementation(libs.logging.slf4JSimple)
+    implementation("org.apache.httpcomponents:httpclient:4.5.13")
+    implementation("info.picocli:picocli:4.6.3")
+    implementation("commons-io:commons-io:2.11.0")
+    implementation("org.json:json:20220924")
+    implementation(libs.jackson.core)
+    implementation(libs.jackson.databind)
+    testImplementation(libs.test.kotestRunnerJunit5)
 }
 
-val mainClassName = "no.elhub.tools.sonarphab.SonarPhabricatorKt"
+val applicationMainClass : String by project
 
-/*
- * Publishing
- * - Create a fat jar for deployment
- * - Run it after the jar command and as part of the assemble task
- */
-val fatJar = task("fatJar", type = Jar::class) {
+application {
+    mainClass.set(applicationMainClass)
+}
+
+val shadowJar by tasks.getting(com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar::class) {
     archiveBaseName.set(rootProject.name)
-    manifest {
-        attributes["Implementation-Title"] = rootProject.name
-        attributes["Implementation-Version"] = rootProject.version
-        attributes["Main-Class"] = mainClassName
-    }
-    from(configurations.compileClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
-    exclude("META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA")
-    with(tasks.jar.get() as CopySpec)
-    mustRunAfter(tasks["jar"])
+    archiveClassifier.set("")
 }
 
-tasks["assemble"].dependsOn(tasks["fatJar"])
+tasks["assemble"].dependsOn(tasks["shadowJar"])
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,19 @@
 rootProject.name = "sonar-phab"
 
+pluginManagement {
+    repositories {
+        maven(url = "https://jfrog.elhub.cloud:443/artifactory/elhub-plugins")
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        maven("https://jfrog.elhub.cloud:443/artifactory/elhub-mvn")
+    }
+
+    versionCatalogs {
+        create("libs") {
+            from("no.elhub.devxp:devxp-versions-catalog:0.2.1")
+        }
+    }
+}
