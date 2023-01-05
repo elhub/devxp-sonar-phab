05ea8dfd5fa2d9c91c1a1bcad279c89d6cd25dc1
Sergii Prodan <serpro@disroot.org>
2023-01-05 10:29:43 +0100

    Bump kotlin-application plugin ver
    Summary: Test dependencyCheck with updated retireJs url
    
    Test Plan: n/a
    
    Reviewers: O5 elhub/devxp, michael.akinde
    
    Reviewed By: O5 elhub/devxp, michael.akinde
    
    Subscribers: teamcity
    
    JIRA Issues: PLAT-4929
    
    Differential Revision: https://phabricator.elhub.cloud/D8883

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("no.elhub.devxp.kotlin-application") version "0.0.5"
+    id("no.elhub.devxp.kotlin-application") version "0.0.6"
 }
 
 description = "Retrieve SonarScan results from Sonarqube and post them to Phabricator."
